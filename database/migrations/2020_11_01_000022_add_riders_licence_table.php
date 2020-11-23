3dd61535131264034c204000504a988419276c60
chengkangzai <pycck@hotmail.com>
2020-11-23 20:12:57 +0800

    NO Default Value !
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- database/migrations/2020_11_01_000022_add_riders_licence_table.php
+++ database/migrations/2020_11_01_000022_add_riders_licence_table.php
@@ -10,7 +10,7 @@ class addRidersLicenceTable extends Migration
     {
         Schema::create('riders_licence', function (Blueprint $table) {
             $table->id();
-            $table->text('class')->default("D")->nullable();
+            $table->text('class')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
             $table->text('IC_number')->nullable();
